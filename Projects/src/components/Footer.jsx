ad8e8dd05ac642f78029b0a28b57de2251eb96a2
Jack <pkhjack2325@gmail.com>
2024-03-18 20:05:26 +0900

    수정

--- Projects/src/components/Footer.jsx
+++ Projects/src/components/Footer.jsx
@@ -3,14 +3,14 @@ import LOGO from '../assets/LOGO.png';
 function Footer() {
 
   return (
-    <footer className="bg-[#1A1A1A] h-[700px] xl:h-[539px] lg:h-[415px] md:h-[320px] sm:h-[246px] m:h-[189px] w-full px-[100px] xl:px-[77px] lg:px-[59.2px] md:px-[45.5px] sm:px-[35px] m:px-[27px] pb-[143px] xl:pb-[110px] lg:pb-[84.6px] md:pb-[65px] sm:pb-[20px] m:pb-[15px] pt-[77px] xl:pt-[59.2px] lg:pt-[45.5px] md:pt-[35px] sm:pt-[27px] m:pt-[21px] flex justify-between items-start ">
+    <footer className=" w-full bg-[#1A1A1A] h-[700px] xl:h-[539px] lg:h-[415px] md:h-[320px] sm:h-[246px] m:h-[189px] px-[100px] xl:px-[77px] lg:px-[59.2px] md:px-[45.5px] sm:px-[35px] m:px-[27px] pb-[143px] xl:pb-[110px] lg:pb-[84.6px] md:pb-[65px] sm:pb-[20px] m:pb-[15px] pt-[77px] xl:pt-[59.2px] lg:pt-[45.5px] md:pt-[35px] sm:pt-[27px] m:pt-[21px] flex justify-between items-start ">
       <div className="logo-container w-[495px] min-w-[100px]"> {/* 로고의 최소 너비 설정 */}
         <a href="/HOME">
           <img src={LOGO} alt="footer logo" className="w-full h-auto" /> {/* 너비를 100%로 설정하여 부모 요소에 맞춰 조정 */}
         </a>
       </div>
 
-      <div className="w-[100%] text-content whitespace-nowrap text-white flex-1 ml-24 text-right mt-[28px] xl:mt-[22px] lg:mt-[17px] md:mt-[13px] sm:mt-[10px] m:mt-[7.7px] font-pretendard-regular text-[24px] lg:text-[20px] md:text-[16px] sm:text-[12px]"> {/* flex-1을 사용하여 나머지 공간을 차지하도록 설정 */}
+      <div className="w-[100%] text-content whitespace-nowrap text-white flex-1 ml-[96px] xl:ml-[74px] lg:ml-[57px] md:ml-[44px] sm:ml-[24px] m:ml-[0px] text-right mt-[28px] xl:mt-[22px] lg:mt-[17px] md:mt-[13px] sm:mt-[10px] m:mt-[7.7px] font-pretendard-regular text-[24px] lg:text-[20px] md:text-[16px] sm:text-[12px] m:text-[9.23px]"> {/* flex-1을 사용하여 나머지 공간을 차지하도록 설정 */}
         <p className='justify-self-end'>경희대학교 생활과학대학 의상학과</p>
         <p className='mb-[21px] xl:mb-[16px] lg:mb-[12px] md:mb-[9.2px] sm:mb-[7.1px] m:mb-[5.5px]'>제44회 졸업작품패션쇼</p>
         <p className='mb-[73px] xl:mb-[56px] lg:mb-[43px] md:mb-[33.1px] sm:mb-[25.4px] m:mb-[19.5px]'>Kyunghee University Clothing & Textile 44th Fashion Show</p>
--- Projects/src/pages/Home.jsx
+++ Projects/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Invite from '../components/Invite';
 
 function HomePage() {
   return (
-    <div className='flex flex-col justify-between flex-1'>
+    <div className='flex flex-col justify-between flex-1 '>
       <Header />
       <main className='mt-[152px] mb-[984px] xl:mt-[112px] xl:mb-[723px] lg:mt-[110px] lg:mb-[700px] md:mt-[73.3px] md:mb-[470px] sm:mt-[49px] sm:mb-[315px] m:mt-[33px] m:mb-[210px]'>
         <Dday />
