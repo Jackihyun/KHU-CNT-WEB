60fb82d357f9be7e525aa8a266f344ab2f363acf
Jack <pkhjack2325@gmail.com>
2024-03-29 17:55:11 +0900

    photo페이지 작업시작

--- Projects/src/pages/PhotoPage.jsx
+++ Projects/src/pages/PhotoPage.jsx
@@ -1,9 +1,15 @@
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import PhotoArticle from "../components/PhotoArticle";
 
 function PhotoPage() {
   return (
     <div className='flex flex-col justify-between flex-1 '>
       <Header />
+      <main>
+        <PhotoArticle />
+      </main>
+      <Footer />
     </div>
   );
 }
