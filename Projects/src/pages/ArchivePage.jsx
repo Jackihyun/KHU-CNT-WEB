879013ec0d483fbb033564083c0515bbc342d156
Yoo yongseok <hpsyong@gmail.com>
2024-04-03 20:55:36 +0900

    아카이브페이지

--- Projects/src/pages/ArchivePage.jsx
+++ Projects/src/pages/ArchivePage.jsx
@@ -1,9 +1,17 @@
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Archive from "../components/Archive";
 
 function ArchivePage() {
   return (
     <div className='flex flex-col justify-between flex-1 '>
-      <Header />
+        <Header/>
+        <main className="mt-[20px]">
+          <div>
+            <Archive/>
+          </div>
+        </main>
+        <Footer/>
     </div>
   );
 }
