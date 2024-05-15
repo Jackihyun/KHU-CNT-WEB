d85ce486bfb37917667eb06fd310934a1c88a741
Yoo yongseok <hpsyong@gmail.com>
2024-05-16 05:57:17 +0900

    thanksto 페이지 이름 그리드로 추가 완료

--- Projects/src/components/AboutMenuThanks.jsx
+++ Projects/src/components/AboutMenuThanks.jsx
@@ -3,98 +3,74 @@ import CommityPhoto from '../assets/졸준위사진.png';
 function AboutMenuThanks() {
   return (
     <div className="flex flex-col mt-[163px] xl:mt-[125px] lg:mt-[95px] md:mt-[74px] sm:mt-[57px] m:mt-[43px]">
-      <div className="flex flex-col justify-center mb-[101px] xl:mb-[77px] lg:mb-[60px] md:mb-[46px] sm:mb-[35px] m:mb-[27px]">
-        <p className="font-pretendard-semibold text-[44px] xl:text-[40px] lg:text-[36px] md:text-[32px] sm:text-[28px] m:text-[24px] mb-[55px] xl:mb-[42px] lg:mb-[32px] md:mb-[25px] sm:mb-[20px] m:mb-[15px]">지도교수님</p>
-        <div className="flex flex-col gap-[16px] xl:gap-[14px] lg:gap-[12px] md:gap-[10px] sm:gap-[8px] m:gap-[6px]">
-          <div className="flex">
-            <p className="font-pretendard-regular text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">감선주 Kam Seon Ju</p>
-          </div>
-          <div className="flex">
-            <p className="font-pretendard-regular text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px] mt-[16px]">우진원 Woo Jin Won</p>
-          </div>
-        </div>
+      <div className="grid grid-cols-1 gap-4 text-[44px] xl:text-[40px] lg:text-[36px] md:text-[32px] sm:text-[28px] m:text-[24px] mb-[50px]">
+        <div className="font-pretendard-semibold">지도교수님</div>
       </div>
+      <div className="grid grid-cols-1 gap-3 text-black text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
+        <div children="font-pretendard-regular">김미숙 Misook Kim</div>
+        <div children="font-pretendard-regular">유신정 Shinjung Yoo</div>
+        <div children="font-pretendard-regular">송화경 HwaKyung Song</div>
+        <div children="font-pretendard-regular">정소진 Sojin Jung</div>
+        <div className="font-pretendard-regular">감선주 Kam Seon Ju</div>
+        <div className="font-pretendard-regular">우진원 Woo Jin Won</div>
+      </div>
+      <div className="mb-[20vh]"></div>
       <div className="flex flex-col justify-center mb-[104px] xl:mb-[80px] lg:mb-[62px] md:mb-[47px] sm:mb-[36px] m:mb-[28px]">
         <p className="font-pretendard-semibold text-[44px] xl:text-[40px] lg:text-[36px] md:text-[32px] sm:text-[28px] m:text-[24px] mb-[50px]">졸업작품준비위원회</p>
         <img src={CommityPhoto} alt="졸준위사진" />
-        <div className="flex flex-col mt-[50px] xl:mt-[38px] lg:mt-[30px] md:mt-[23px] sm:mt-[18px] m:mt-[13px]">
-          <div className="flex mb-[20px] text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
-            <p className="w-[407px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-semibold">졸업작품준비위원장</p>
-            <p className='font-pretendard-regular'>김희연 Heeyeon Kim</p>
-          </div>
-          <div className="flex mb-[20px] xl:mb-[18px] lg:mb-[16px] md:mb-[14px] sm:mb-[12px] m:mb-[10px] text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
-            <p className="w-[407px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-semibold">졸업작품준비부위원장</p>
-            <p className='font-pretendard-regular'>김규리 Gyuri Kim</p>
-          </div>
-          <div className="flex mb-[20px] xl:mb-[18px] lg:mb-[16px] md:mb-[14px] sm:mb-[12px] m:mb-[10px] text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
-            <p className="w-[407px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-semibold">졸업작품준비회 총무</p>
-            <p className='font-pretendard-regular'>홍채희 Chaehui Hong</p>
-          </div>
-          <div className="flex mb-[20px] xl:mb-[18px] lg:mb-[16px] md:mb-[14px] sm:mb-[12px] m:mb-[10px] text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
-            <p className="w-[407px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-semibold">졸업작품준비회 회계</p>
-            <p className='font-pretendard-regular'>조연진 Yeonjin Cho</p>
-          </div>
-          <div className="flex mb-[28px] xl:mb-[26px] lg:mb-[24px] md:mb-[22px] sm:mb-[20px] m:mb-[18px] text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
-            <p className="w-[407px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-semibold">4학년 학과 대표</p>
-            <p className='font-pretendard-regular'>사한나 Hanna Sa</p>
-          </div>
-          <div className="flex mb-[46px] xl:mb-[42px] lg:mb-[38px] md:mb-[34px] sm:mb-[30px] m:mb-[26px] text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
-            <p className="w-[407px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-semibold">졸업작품준비회 마케팅팀</p>
-            <div className="flex flex-col gap-[16px] xl:gap-[14px] lg:gap-[12px] md:gap-[10px] sm:gap-[8px] m:gap-[6px]">
-              <div className="flex text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
-                <p className='w-[410px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-regular'>김규리 Gyuri Kim</p>
-                <p className='font-pretendard-regular'>김희연 Heeyeon Kim</p>
-              </div>
-              <div className="flex text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
-                <p className='w-[410px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-regular'>민시은 Sieun Min</p>
-                <p className='font-pretendard-regular'>박슬빈 Seulbin Park</p>
-              </div>
-              <div className="flex text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
-                <p className='w-[410px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-regular'>송겸승 Gyeomseung Song</p>
-                <p className='font-pretendard-regular'>조연진 Yeonjin Cho</p>
-              </div>
-            </div>
-          </div>
-          <div className="flex mb-[46px] xl:mb-[35px] lg:mb-[27px] md:mb-[21px] sm:mb-[16px] m:mb-[12px]">
-            <p className="w-[407px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-semibold text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">졸업작품준비회 편집디자인팀</p>
-            <div className="flex flex-col gap-[16px] xl:gap-[14px] lg:gap-[12px] md:gap-[10px] sm:gap-[8px] m:gap-[6px]">
-              <div className="flex text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
-                <p className='w-[410px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-regular'>강민영 Minyoung Kang</p>
-                <p className='font-pretendard-regular'>김규리 Gyuri Kim</p>
-              </div>
-              <div className="flex text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
-                <p className='w-[410px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-regular'>김민지 Minji Kim</p>
-                <p className='font-pretendard-regular'>김희연 Heeyeon Kim</p>
-              </div>
-              <div className="flex text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
-                <p className='w-[410px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-regular'>이유나 Yu-na Lee</p>
-                <p className='font-pretendard-regular'>이지우 Jiwoo Lee</p>
-              </div>
-              <div className="flex">
-                <p className='w-[410px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-regular text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]'>홍채희 Chaehui Hong</p>
-              </div>
-            </div>
-          </div>
-          <div className="flex">
-            <p className="w-[407px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-semibold text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">졸업작품준비회 미디어팀</p>
-            <div className="flex flex-col gap-[16px] xl:gap-[14px] lg:gap-[12px] md:gap-[10px] sm:gap-[8px] m:gap-[6px]">
-              <div className="flex text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
-                <p className='w-[410px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-regular'>강민영 Minyoung Kang</p>
-                <p className='font-pretendard-regular'>김규리 Gyuri Kim</p>
-              </div>
-              <div className="flex text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
-                <p className='w-[410px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-regular'>김희연 Heeyeon Kim</p>
-                <p className='font-pretendard-regular'>박슬빈 Seulbin Park</p>
-              </div>
-              <div className="flex text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
-                <p className='w-[410px] xl:w-[320px] lg:w-[250px] md:w-[195px] sm:w-[162px] m:w-[130px] font-pretendard-regular'>사한나 Hanna Sa</p>
-                <p className='font-pretendard-regular'>이유나 Yu-na Lee</p>
-              </div>
-            </div>
-          </div>
+        <div className="grid grid-cols-3 gap-4 mt-[5%] mb-[20px] text-[20px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
+          <div className=" font-pretendard-semibold">졸업작품준비위원장</div>
+          <div className="font-pretendard-regular">김희연 Heeyeon Kim</div>
+          <div className=""></div>
+          <div className='font-pretendard-semibold'>졸업작품준비부위원장</div>
+          <div className='font-pretendard-regular'>김규리 Gyuri Kim</div>
+          <div className=""></div>
+          <div className='font-pretendard-semibold'>졸업작품준비회 총무</div>
+          <div className='font-pretendard-regular'>홍채희 Chaehui Hong</div>
+          <div className=""></div>
+          <div className='font-pretendard-semibold'>졸업작품준비회 회계</div>
+          <div className='font-pretendard-regular'>조연진 Yeonjin Cho</div>
+          <div className=""></div>
+          <div className='font-pretendard-semibold'>4학년 학과 대표</div>
+          <div className='font-pretendard-regular'>사한나 Hanna Sa</div>
+        </div>
+        <div className="grid grid-cols-3 gap-4 mt-[5%] mb-[20px] text-[20px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
+          <div className=" font-pretendard-semibold">졸업작품준비회 마케팅팀</div>
+          <div className="font-pretendard-regular">김규리 Gyuri Kim</div>
+          <div className="font-pretendard-regular">김희연 Heeyeon Kim</div>
+          <div className=""></div>
+          <div className="font-pretendard-regular">민시은 Sieun Min</div>
+          <div className="font-pretendard-regular">박슬빈 Seulbin Park</div>
+          <div className=""></div>
+          <div className="font-pretendard-regular">송겸승 Gyeomseung Song</div>
+          <div className="font-pretendard-regular">조연진 Yeonjin Cho</div>
+        </div>
+        <div className="grid grid-cols-3 gap-4 mt-[5%] mb-[20px] text-[20px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
+          <div className="font-pretendard-semibold">졸업작품준비회 편집디자인팀</div>
+            <div className="font-pretendard-regular">강민영 Minyoung Kang</div>
+            <div className="font-pretendard-regular">김규리 Gyuri Kim</div>
+            <div className=""></div>
+            <div className="font-pretendard-regular">김민지 Minji Kim</div>
+            <div className="font-pretendard-regular">김희연 Heeyeon Kim</div>
+            <div className=""></div>
+            <div className="font-pretendard-regular">이유나 Yu-na Lee</div>
+            <div className="font-pretendard-regular">이지우 Jiwoo Lee</div>
+            <div className=""></div>
+            <div className="font-pretendard-regular">홍채희 Chaehui Hong</div>
+        </div>
+        <div className="grid grid-cols-3 gap-4 mt-[5%] mb-[20px] text-[20px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
+          <div className="font-pretendard-semibold">졸업작품준비회 미디어팀</div>
+          <div className="font-pretendard-regular">강민영 Minyoung Kang</div>
+          <div className="font-pretendard-regular">김규리 Gyuri Kim</div>
+          <div className=""></div>
+          <div className="font-pretendard-regular">김희연 Heeyeon Kim</div>
+          <div className="font-pretendard-regular">박슬빈 Seulbin Park</div>
+          <div className=""></div>
+          <div className="font-pretendard-regular">사한나 Hanna Sa</div>
+          <div className="font-pretendard-regular">이유나 Yu-na Lee</div>
         </div>
       </div>
-      <div className="flex flex-col justify-center mb-[451px] xl:mb-[347px] lg:mb-[267px] md:mb-[205px] sm:mb-[158px] m:mb-[121px]">
+      {/* <div className="flex flex-col justify-center mb-[451px] xl:mb-[347px] lg:mb-[267px] md:mb-[205px] sm:mb-[158px] m:mb-[121px]">
         <p className="font-pretendard-semibold text-[44px] xl:text-[40px] lg:text-[36px] md:text-[32px] sm:text-[28px] m:text-[24px] mb-[50px]">제작 지원</p>
         <div className="flex flex-col gap-[20px] xl:gap-[18px] lg:gap-[16px] md:gap-[14px] sm:gap-[12px] m:gap-[10px]">
           <div className="flex">
@@ -122,6 +98,40 @@ function AboutMenuThanks() {
             <p className="font-pretendard-regular text-[18px] xl:text-[16px] lg:text-[14px] md:text-[12px] sm:text-[10px] m:text-[8px] flex items-center text-[#A1A1A1]">	&gt; Contact</p>
           </div>
         </div>
+      </div> */}
+      <div className="grid grid-cols-1 gap-4 text-[44px] xl:text-[40px] lg:text-[36px] md:text-[32px] sm:text-[28px] m:text-[24px] mb-[50px]">
+        <div className="font-pretendard-semibold">제작 지원</div>
+      </div>
+      <div className="grid grid-cols-3 gap-4 mb-[20px] text-[20px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">
+        <div className=" font-pretendard-semibold">웹사이트 개발</div>
+        <div className="font-pretendard-regular">박기현</div>
+        <div className="text-[#A1A1A1] "> &gt; Contact</div>
+        <div className=''></div>
+        <div className="font-pretendard-regular">유용석</div>
+        <div className="text-[#A1A1A1] "> &gt; Contact</div>
+
+        <div className=" font-pretendard-semibold">화이트 프로젝트 영상 제작</div>
+        <div className='font-pretendard-regular'>ROUGH.LAB</div>
+        <div className="text-[#A1A1A1] "> &gt; Contact</div>
+
+        <div className=" font-pretendard-semibold">패션쇼 영상 제작</div>
+        <div className='flex items-center'>
+          <div className='font-pretendard-regular'>서하영</div>
+          <div className="text-[13px] ml-2 ">전체, Team A,F</div>
+        </div>
+        <div className="text-[#A1A1A1] "> &gt; Contact</div>
+        <div className=''></div>
+        <div className='flex items-center'>
+          <div className='font-pretendard-regular'>윤지영</div>
+          <div className="text-[13px] ml-2 ">Team B, D, E</div>
+        </div>
+        <div className="text-[#A1A1A1] "> &gt; Contact</div>
+        <div className=''></div>
+        <div className='flex items-center'>
+          <div className='font-pretendard-regular'>김은정</div>
+          <div className="text-[13px] ml-2 ">Team C</div>
+        </div>
+        <div className="text-[#A1A1A1] "> &gt; Contact</div>
       </div>
     </div >
   );
