0ca8b3341468ac216112becacaa748f5386ae12d
Yoo yongseok <hpsyong@gmail.com>
2024-05-16 13:29:36 +0900

    thanksto 페이지 마진조절

--- Projects/src/components/AboutMenuThanks.jsx
+++ Projects/src/components/AboutMenuThanks.jsx
@@ -14,7 +14,7 @@ function AboutMenuThanks() {
         <div className="font-pretendard-regular">감선주 Kam Seon Ju</div>
         <div className="font-pretendard-regular">우진원 Woo Jin Won</div>
       </div>
-      <div className="mb-[20vh]"></div>
+      <div className="mb-[15vh]"></div>
       <div className="flex flex-col justify-center mb-[104px] xl:mb-[80px] lg:mb-[62px] md:mb-[47px] sm:mb-[36px] m:mb-[28px]">
         <p className="font-pretendard-semibold text-[44px] xl:text-[40px] lg:text-[36px] md:text-[32px] sm:text-[28px] m:text-[24px] mb-[50px]">졸업작품준비위원회</p>
         <img src={CommityPhoto} alt="졸준위사진" />
@@ -70,35 +70,6 @@ function AboutMenuThanks() {
           <div className="font-pretendard-regular">이유나 Yu-na Lee</div>
         </div>
       </div>
-      {/* <div className="flex flex-col justify-center mb-[451px] xl:mb-[347px] lg:mb-[267px] md:mb-[205px] sm:mb-[158px] m:mb-[121px]">
-        <p className="font-pretendard-semibold text-[44px] xl:text-[40px] lg:text-[36px] md:text-[32px] sm:text-[28px] m:text-[24px] mb-[50px]">제작 지원</p>
-        <div className="flex flex-col gap-[20px] xl:gap-[18px] lg:gap-[16px] md:gap-[14px] sm:gap-[12px] m:gap-[10px]">
-          <div className="flex">
-            <p className="w-[527px] xl:w-[405px] lg:w-[312px] md:w-[240px] sm:w-[185px] m:w-[142px] font-pretendard-semibold text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">웹사이트 개발</p>
-
-            <div className="flex flex-col justify-between">
-              <div className="flex mb-[10px] xl:mb-[8px] lg:mb-[6px] md:mb-[4px] sm:mb-[2px] m:mb-[1px]">
-                <p className='w-[290px] xl:w-[223px] lg:w-[172px] md:w-[132px] sm:w-[102px] m:w-[78px] font-pretendard-regular text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]'>박기현 Kihyun Park</p>
-                <p className="font-pretendard-regular text-[18px] xl:text-[16px] lg:text-[14px] md:text-[12px] sm:text-[10px] m:text-[8px] flex items-center text-[#A1A1A1]">	&gt; Contact</p>
-              </div>
-              <div className="flex">
-                <p className='w-[290px] xl:w-[223px] lg:w-[172px] md:w-[132px] sm:w-[102px] m:w-[78px] font-pretendard-regular text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]'>유용석 Yongseok Yoo</p>
-                <p className="font-pretendard-regular text-[18px] xl:text-[16px] lg:text-[14px] md:text-[12px] sm:text-[10px] m:text-[8px] flex items-center text-[#A1A1A1]">	&gt; Contact</p>
-              </div>
-            </div>
-          </div>
-          <div className="flex">
-            <p className="w-[527px] xl:w-[405px] lg:w-[312px] md:w-[240px] sm:w-[185px] m:w-[142px] font-pretendard-semibold text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">화이트 프로젝트 영상 제작</p>
-            <p className='w-[290px] xl:w-[223px] lg:w-[172px] md:w-[132px] sm:w-[102px] m:w-[78px] font-pretendard-regular text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]'>ROUGH.LAB</p>
-            <p className="font-pretendard-regular text-[18px] xl:text-[16px] lg:text-[14px] md:text-[12px] sm:text-[10px] m:text-[8px] flex items-center text-[#A1A1A1]">	&gt; Contact</p>
-          </div>
-          <div className="flex">
-            <p className="w-[527px] xl:w-[405px] lg:w-[312px] md:w-[240px] sm:w-[185px] m:w-[142px] font-pretendard-semibold text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]">패션쇼 영상 제작</p>
-            <p className='w-[290px] xl:w-[223px] lg:w-[172px] md:w-[132px] sm:w-[102px] m:w-[78px] font-pretendard-regular text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] m:text-[14px]'>추가하기</p>
-            <p className="font-pretendard-regular text-[18px] xl:text-[16px] lg:text-[14px] md:text-[12px] sm:text-[10px] m:text-[8px] flex items-center text-[#A1A1A1]">	&gt; Contact</p>
-          </div>
-        </div>
-      </div> */}
       <div className="grid grid-cols-1 gap-4 text-[44px] xl:text-[40px] lg:text-[36px] md:text-[32px] sm:text-[28px] m:text-[24px] mb-[50px]">
         <div className="font-pretendard-semibold">제작 지원</div>
       </div>
@@ -133,6 +104,7 @@ function AboutMenuThanks() {
         </div>
         <div className="text-[#A1A1A1] "> &gt; Contact</div>
       </div>
+      <div className='mb-[15vh]'></div>
     </div >
   );
 }
